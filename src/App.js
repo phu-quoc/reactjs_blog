fafa8a46f4094d534b5d1d44330c712a15398fe8
Le Phu Quoc <tygovip119@gmail.com>
2023-08-01 10:41:42 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,34 +1,34 @@
-// import Home from "./pages/home/Home";
-// import Single from "./pages/single/Single";
-// import TopBar from "./components/topbar/TopBar";
-// import Write from "./pages/write/Write";
-// import Settings from "./pages/settings/Settings";
-// import Login from "./pages/login/Login";
-// import Register from "./pages/register/Register";
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import TopBar from "./components/topbar/TopBar";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
 import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
   const user = false;
   return (
     <Router>
-      {/*<TopBar />*/}
-      {/*<Routes>*/}
-      {/*  <Route exact path='/' element={<Home />} />*/}
-      {/*</Routes>*/}
-      {/*<Routes>*/}
-      {/*  <Route path='/register' element={ user ? <Home /> : <Register />} />*/}
-      {/*</Routes>*/}
-      {/*<Routes>*/}
-      {/*  <Route path='/login' element={ user ? <Home /> : <Login />} />*/}
-      {/*</Routes>*/}
-      {/*<Routes>*/}
-      {/*  <Route path='/write' element={ user ? <Write /> : <Login />} />*/}
-      {/*</Routes>*/}
-      {/*<Routes>*/}
-      {/*  <Route path='/settings' element={ user ? <Settings /> : <Login />} />*/}
-      {/*</Routes>*/}
-      {/*<Routes>*/}
-      {/*  <Route path='/post/:slug' element={<Single />} />*/}
-      {/*</Routes>*/}
+      <TopBar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path='/register' element={ user ? <Home /> : <Register />} />
+      </Routes>
+      <Routes>
+        <Route path='/login' element={ user ? <Home /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/write' element={ user ? <Write /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/settings' element={ user ? <Settings /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/post/:slug' element={<Single />} />
+      </Routes>
     </Router>
   );
 }
