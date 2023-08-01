6a8e7c8f193b686c7d121986c3096abfca6723b4
Le Phu Quoc <tygovip119@gmail.com>
2023-08-01 09:44:28 +0700

    Init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import logo from './logo.svg';
-import './App.css';
-
+import Home from "./pages/home/Home";
+import Single from "./pages/single/Single";
+import TopBar from "./components/topbar/TopBar";
+import Write from "./pages/write/Write";
+import Settings from "./pages/settings/Settings";
+import Login from "./pages/login/Login";
+import Register from "./pages/register/Register";
+import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
 function App() {
+  const user = false;
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <TopBar />
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+      <Routes>
+        <Route path='/register' element={ user ? <Home /> : <Register />} />
+      </Routes>
+      <Routes>
+        <Route path='/login' element={ user ? <Home /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/write' element={ user ? <Write /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/settings' element={ user ? <Settings /> : <Login />} />
+      </Routes>
+      <Routes>
+        <Route path='/post/:slug' element={<Single />} />
+      </Routes>
+    </Router>
   );
 }
 
